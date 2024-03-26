12db10cb02d6f9b2648dbec81a7e7577714fdf4a
ryan <ryan37549@gmail.com>
2024-03-26 08:51:33 -0500

    minimum width of sidebar set to image width

--- priceGUI.py
+++ priceGUI.py
@@ -79,19 +79,20 @@ root.minsize(int(width*0.75),int(height*0.90))
 
 
 image = Image.open('Scar20.jpeg')
+iWidth, iHeight = image.size
 photo = ImageTk.PhotoImage(image)
-outputs = Canvas(root,bd=0,highlightthickness=0.5,bg='#283030',height=str(height*0.25),width=width*0.10)
+outputs = Canvas(root,bd=0,highlightthickness=0.5,bg='#283030',height=str(height*0.25),width=iWidth*0.10)
 displayImage = Label(outputs,image=photo,bd=0,highlightthickness=0, width=265,height=200,bg='#206963')
 itemMenu = OptionMenu(outputs,selectedOption,*options)
 graphFrame = Canvas(root,bd=0,highlightthickness=0.5,bg='#283030',height=str(height*0.25),width=width)
 button = Button(root, text = "testButton",command = get_prices)
 graphWidth= graphFrame.winfo_width()
 
-displayImage.pack(anchor='nw',pady=10,padx=5)
+displayImage.pack(anchor='nw',pady=20,padx=5)
 itemMenu.pack(anchor='w',padx=10)
 graphFrame.place(relx=0.005,rely=1.0, anchor='sw', x=-0,y=-10,relheight=0.25, relwidth=0.99)
 button.pack(anchor='e',padx=10)
-outputs.place(relx=0.005,rely=0.05,anchor='nw', x=-0, y=-50,relwidth=0.2,relheight=0.75)
+outputs.place(relx=0.005,rely=0.05,anchor='nw', x=-0, y=-50,relheight=0.75)
 
 
 
